08248dd85a7575a1665ac1564025ae9f241d8685
Benjamin Congdon <bencongdon96@gmail.com>
2017-05-18 14:54:44 -0700

    CORS support for backend

--- everyday/__init__.py
+++ everyday/__init__.py
@@ -3,8 +3,10 @@ from flask import Flask
 from flask_restful import Api
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 settings = os.getenv('APP_SETTINGS', 'everyday.config.DevelopmentConfig')
 app.config.from_object(settings)
