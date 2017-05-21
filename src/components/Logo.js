2953e033e56c097d3a662e366cbbc6339081edac
Benjamin Congdon <bencongdon96@gmail.com>
2017-05-21 15:30:51 -0700

    Logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import FontAwesome from 'react-fontawesome'
+
+class Logo extends Component {
+  render() {
+    return (
+      <div><FontAwesome name='calendar-check-o' /> Everyday</div>
+    )
+  }
+}
+
+export default Logo
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import Logo from '../Logo'
 
 class HomePage extends Component {
   render() {
     return (
-      <div>Hello world! This is the home page route.</div>
+
+      <div><Logo />Hello world! This is the home page route.</div>
     )
   }
 }
