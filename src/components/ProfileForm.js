dadbe618d9fc6d171e43f21442653003045294b3
Benjamin Congdon <bencongdon96@gmail.com>
2017-05-26 10:03:23 -0700

    UI for profile + profile editing

---
+++ src/components/ProfileForm.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Field, reduxForm } from 'redux-form'
+import { PropTypes } from 'prop-types'
+import { Button, Form, Divider } from 'semantic-ui-react'
+import SemanticReduxFormField from './form/SemanticReduxFormField'
+import ErrorMessage from './ErrorMessage'
+
+const form = reduxForm({
+  form: 'editProfile',
+  validate
+})
+
+function validate (formProps) {
+  const errors = {}
+
+  return errors
+}
+
+class EntryForm extends Component {
+  static propTypes = {
+    handleSubmit: PropTypes.func.isRequired,
+    errorMessage: PropTypes.string
+  }
+
+  constructor (props) {
+    super(props)
+    this.handleFormSubmit = this.handleFormSubmit.bind(this)
+  }
+
+  handleFormSubmit (formProps) {
+    // TODO: Connect to profile edit action
+  }
+
+  renderAlert () {
+    if (!this.props.errorMessage) {
+      return null
+    }
+
+    return (
+      <ErrorMessage message={this.props.errorMessage} />
+    )
+  }
+
+  render () {
+    return (
+      <Form warning error onSubmit={this.props.handleSubmit(this.handleFormSubmit)}>
+        {this.renderAlert()}
+        <Form.Field>
+          <label>Email</label>
+          <Field component={SemanticReduxFormField} as={Form.Input} name='email' placeholder='Email' />
+        </Form.Field>
+        <Form.Field>
+          <label>Name</label>
+          <Field component={SemanticReduxFormField} as={Form.Input} name='name' placeholder='What should we call you?' />
+        </Form.Field>
+        <Form.Field>
+          <label>New Password</label>
+          <Field component={SemanticReduxFormField} as={Form.Input} name='new_password' placeholder='Password' type='password' />
+        </Form.Field>
+        <Divider section />
+        <Form.Field>
+          <label>Password</label>
+          <Field component={SemanticReduxFormField} as={Form.Input} name='old_password' placeholder='Password' type='password' />
+        </Form.Field>
+        <Button type='submit' primary>Save</Button>
+      </Form>
+    )
+  }
+}
+
+function mapStateToProps (state) {
+  return {
+    errorMessage: state.entry.error
+  }
+}
+
+export default connect(mapStateToProps)(form(EntryForm))
--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { Container, Card } from 'semantic-ui-react'
+import { Card, Grid, Segment, Header } from 'semantic-ui-react'
 import UserNavbar from '../components/UserNavbar'
 import { connect } from 'react-redux'
 import { PropTypes } from 'prop-types'
 import Gravatar from '../components/Gravatar'
+import ProfileForm from '../components/ProfileForm'
 
 class ProfilePage extends Component {
   static propTypes = {
@@ -14,16 +15,24 @@ class ProfilePage extends Component {
     return (
       <div>
         <UserNavbar />
-        <Container>
-          <Card>
-            <Gravatar email={this.props.user.email} size={720} avatar={false} />
-            <Card.Content>
-              <Card.Header>{this.props.user.name}</Card.Header>
-              <Card.Meta>Joined in 2017</Card.Meta>
-              <Card.Description>{this.props.user.name} has composed {5} entries on EveryDay.</Card.Description>
-            </Card.Content>
-          </Card>
-        </Container>
+        <Grid style={{maxWidth: 900, margin: '0 auto'}} centered textAlign='left'>
+          <Grid.Column width={5} style={{minWidth: 200}}>
+            <Card>
+              <Gravatar email={this.props.user.email} size={720} avatar={false} />
+              <Card.Content>
+                <Card.Header>{this.props.user.name}</Card.Header>
+                <Card.Meta>Joined in 2017</Card.Meta>
+                <Card.Description>{this.props.user.name} has composed {5} entries on EveryDay.</Card.Description>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+          <Grid.Column floated='right' computer={10} tablet={10} mobile={16}>
+            <Segment>
+              <Header size='medium'>Edit Profile</Header>
+              <ProfileForm initialValues={this.props.user} />
+            </Segment>
+          </Grid.Column>
+        </Grid>
       </div>
     )
   }
