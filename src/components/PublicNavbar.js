4b078286e2ce6380654e892eb933fd4823d62262
Benjamin Congdon <bencongdon96@gmail.com>
2017-06-10 12:35:42 -0700

    Improved mobile version of public navbar (still imperfect)

--- src/components/PublicNavbar.js
+++ src/components/PublicNavbar.js
@@ -1,27 +1,47 @@
 import React, { Component } from 'react'
 import Logo from './Logo'
 import { Link } from 'react-router-dom'
-import { Menu } from 'semantic-ui-react'
+import { Menu, Grid, Dropdown } from 'semantic-ui-react'
 import './PublicNavbar.css'
 
 class PublicNavbar extends Component {
   render () {
     return (
-      <Menu stackable borderless>
-        <Menu.Item>
-          <Link to='/' className='navbar-logo-link'>
-            <Logo />
-          </Link>
-        </Menu.Item>
-        <Menu.Menu position='right'>
-          <Menu.Item>
-            <Link to='/register'>Sign Up</Link>
-          </Menu.Item>
-        </Menu.Menu>
-        <Menu.Item>
-          <Link to='/login'>Log In</Link>
-        </Menu.Item>
-      </Menu>
+      <Grid>
+        <Grid.Row className='computer tablet only'>
+          <Menu borderless style={{width: '100%', marginLeft: 15, marginRight: 15}}>
+            <Menu.Item>
+              <Link to='/' className='navbar-logo-link'>
+                <Logo />
+              </Link>
+            </Menu.Item>
+            <Menu.Menu position='right'>
+              <Menu.Item>
+                <Link to='/register'>Sign Up</Link>
+              </Menu.Item>
+            </Menu.Menu>
+            <Menu.Item>
+              <Link to='/login'>Log In</Link>
+            </Menu.Item>
+          </Menu>
+        </Grid.Row>
+        <Grid.Row className='mobile only'>
+          <Menu borderless style={{width: '100%', marginLeft: 15, marginRight: 15}}>
+            <Menu.Item>
+              <Link to='/' className='navbar-logo-link'>
+                <Logo />
+              </Link>
+            </Menu.Item>
+            <Menu.Menu position='right' />
+            <Dropdown pointing='top right' item icon='content'>
+              <Dropdown.Menu>
+                <Dropdown.Item><Link to='/register'>Sign Up</Link></Dropdown.Item>
+                <Dropdown.Item><Link to='/login'>Log In</Link></Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Menu>
+        </Grid.Row>
+      </Grid>
     )
   }
 }
